96e20c66c874316953b30329285fb6510c1affa5
Shawn He <shawnhe1991@gmail.com>
2023-03-19 13:16:24 -0400

    complete timer

---
+++ typewriter.js
@@ -0,0 +1,10 @@
+const sentence = "hello there from lighthouse labs"
+
+timer = 50;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char)
+  }, (timer +=50))//since the loop is complete at the same time, because the delay is all the same, it wont print, so you must create a timer loop
+}
+console.log(timer);
+  
