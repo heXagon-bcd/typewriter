7815523221654f9ef0434b39708cacbce2adc629
Shawn He <shawnhe1991@gmail.com>
2023-03-19 13:19:15 -0400

    new line added

--- typewriter.js
+++ typewriter.js
@@ -3,7 +3,8 @@ const sentence = "hello there from lighthouse labs"
 timer = 50;
 for (const char of sentence) {
   setTimeout(() => {
-    process.stdout.write(char)
+    process.stdout.write(`${char}`)//same line
+    process.stdout.write(`\n ${char}`)//new line
   }, (timer +=50))//since the loop is complete at the same time, because the delay is all the same, it wont print, so you must create a timer loop
 }
 console.log(timer);
